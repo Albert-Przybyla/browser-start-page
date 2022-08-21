6466ae598c6244cec37dffe10eef28d2fff6e772
APrzybyla <106275293+Albert-Przybyla@users.noreply.github.com>
2022-08-21 21:31:20 +0200

    change typed.js to vue-writer, create Home & Claim

---
+++ src/components/Claim.vue
@@ -0,0 +1,32 @@
+<script setup>
+import VueWriter from 'vue-writer'
+</script>
+
+
+<template>
+    <div class="wrapper">
+         <VueWriter :array="['Hello World']" />
+    </div>
+</template>
+
+<script>
+    export default {
+    name: "Clime",
+    components: {
+        VueWriter
+    },
+    data(){
+        return {
+                
+        }
+    },
+
+}
+</script>
+
+
+
+<style scoped>
+
+
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,24 @@
+<script setup>
+    import claim from "@/components/Claim.vue"
+</script>
+
+<template>
+    <header>
+    <claim/>
+    </header>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data(){
+    return {
+        components: {claim},
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
