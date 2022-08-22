bc62cc5c4f5fcc4c5f31f9d7a1775b70b17a6ad1
APrzybyla <106275293+Albert-Przybyla@users.noreply.github.com>
2022-08-22 16:54:15 +0200

    create shortcut component and style changes

---
+++ src/components/Shortcuts.vue
@@ -0,0 +1,45 @@
+<template>
+<div class="wrapper">
+    <div class="shortcut">
+        
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "Shortcuts",
+  data(){
+    return {
+
+    }
+  },
+
+};
+</script>
+
+<style scoped>
+
+
+    .material-symbols-outlined {
+    font-variation-settings:
+    'FILL' 0,
+    'wght' 700,
+    'GRAD' 0,
+    'opsz' 48,
+    }
+
+    @media (min-width: 760px){
+
+    }
+
+    @media (min-width: 1024px){
+
+    }
+
+    @media (min-width: 2524px){
+
+    }
+    
+
+</style>
